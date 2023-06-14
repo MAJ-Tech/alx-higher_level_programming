13ba2998f2f3a1684c71aff4edaf696314d52253
MAJ-Tech <modiajawo24@gmail.com>
2023-06-13 19:34:15 -0700

    More returns!

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+
+def multiple_returns(sentence):
+    """Returns tuple with len of string and first char"""
+    len_sen = len(sentence)
+    if sentence:
+        first_char = sentence[0]
+    else:
+        first = None
+    return (len_sen, first_char)
