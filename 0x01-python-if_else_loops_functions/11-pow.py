0e61b8ae1de22f484505aaa156bc0cf264a10b3d
MAJ-Tech <modiajawo24@gmail.com>
2023-06-17 06:08:05 -0700

    Power

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -2,17 +2,15 @@
 
 
 def pow(a, b):
-     """i want to create a variable that holds result of product."""
      res = 1
-     base = 1
-     numb = 0
      if b < 0:
-         numb = b
-         b = (-1) * b
-         for i in range(b):
-             res *= a
-             base = res * res
-        if numb  < 0:
-            res /= base
-        return res
+          a = 1/a
+          b = -b
+     while b > 0:
+          if b % 2 == 1:
+               res *= a
+          a *= a
+          b //= 2
+     return res
 
+                    
