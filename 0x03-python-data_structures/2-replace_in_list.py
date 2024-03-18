281fd91ce74496d734ef6ccdf0ba75d71cba9c5f
= <=>
2024-03-17 17:59:55 -0700

    replaces an element of a list at a specific position

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -2,8 +2,6 @@
 
 def replace_in_list(my_list, idx, element):
     """function that replaces an element of a list at a specific position"""
-    if idx < 0 or idx > len(my_list) - 1:
-        return my_list
-    else:
+    if 0 <= idx < len(my_list):
         my_list[idx] = element
-        return my_list
+    return my_list
