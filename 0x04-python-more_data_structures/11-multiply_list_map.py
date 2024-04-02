72d3795fdb9915c4c27f5f61c1abf83a4560641b
= <=>
2024-04-02 03:38:53 -0700

    Update

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -5,4 +5,5 @@ def multiply_list_map(my_list=[], number=0):
     Return a list of all values multiply by a number
     without using a loop
     """
-    return list(map(lambda x: x * number, my_list))
+    new_list = list(map(lambda x: x * number, my_list))
+    return new_list
