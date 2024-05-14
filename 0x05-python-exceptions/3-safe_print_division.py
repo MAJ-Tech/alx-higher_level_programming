6e4c2c3a9ff1d44e8404ab8d546dc87107db9f85
MAJ-Tech <modiajawo24@gmail.com>
2024-05-14 15:19:40 -0700

    Safe print division

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def safe_print_division(a,b):
+    '''Safe print Division'''
+    result = None
+
+    try:
+        result = a / b
+
+    except ZeroDivisionError:
+        result = None
+
+    finally:
+        print("Inside result: {}".format(result))
+
+    return result
