0de1f09d607c8ffe908807b9edd0e2ab9e148a48
MAJ-Tech <modiajawo24@gmail.com>
2024-05-20 04:50:52 -0700

    Class difination in python

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,10 @@
+"""
+0-square.py
+
+This is a module for practicing my first python classes and methods
+"""
+class Square:
+    """
+    This is an empty class that defines a square
+    """
+    pass
